e9ce4984e5c35d1aff2f30b2a3ddb595cb03b2cd
Nelson Nobre <technobre@gmail.com>
2022-01-29 10:01:56 +0000

    Added new extensions to convert length

--- tests/PowerUtils.Geolocation.Tests/ConversionExtensionsTests/FromMeterToTests.cs
+++ tests/PowerUtils.Geolocation.Tests/ConversionExtensionsTests/FromMeterToTests.cs
@@ -3,6 +3,156 @@
 [Trait("Type", "Extensions")]
 public class FromMeterToTests
 {
+    [Theory(DisplayName = "Converting int numbers in meters to kilometers")]
+    [InlineData(45_000, 45)]
+    [InlineData(423_000, 423)]
+    [InlineData(42_331_000, 42_331)]
+    public void Int_Meter_Kilometer(int meter, int expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting uint numbers in meters to kilometers")]
+    [InlineData(45_000, 45)]
+    [InlineData(423_000, 423)]
+    [InlineData(42_331_000, 42_331)]
+    public void UInt_Meter_Kilometer(uint meter, uint expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting long numbers in meters to kilometers")]
+    [InlineData(45_000, 45)]
+    [InlineData(423_000, 423)]
+    [InlineData(42_331_000, 42_331)]
+    public void Long_Meter_Kilometer(long meter, long expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting ulong numbers in meters to kilometers")]
+    [InlineData(45_000, 45)]
+    [InlineData(423_000, 423)]
+    [InlineData(42_331_000, 42_331)]
+    public void ULong_Meter_Kilometer(ulong meter, ulong expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting float numbers in meters to kilometers")]
+    [InlineData(45_120, 45.12)]
+    [InlineData(423_457, 423.457)]
+    [InlineData(11_423_457, 11_423.457)]
+    public void Float_Meter_Kilometer(float meter, float expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting double numbers in meters to kilometers")]
+    [InlineData(45_120, 45.12)]
+    [InlineData(423_457, 423.457)]
+    [InlineData(11_423_457, 11_423.457)]
+    public void Double_Meter_Kilometer(double meter, double expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting decimal numbers in meters to kilometers")]
+    [InlineData(45_120, 45.12)]
+    [InlineData(423_457, 423.457)]
+    [InlineData(11_423_457, 11_423.457)]
+    public void Decimal_Meter_Kilometer(decimal meter, decimal expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting float numbers in meters to miles")]
+    [InlineData(11_423.457, 7.09820461)]
+    [InlineData(1.154, 0.000717062154)]
+    [InlineData(221.24, 0.137472123)]
+    public void Float_Meter_Mile(float meter, float expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToMile();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting double numbers in meters to miles")]
+    [InlineData(11_423.457, 7.098204899547)]
+    [InlineData(1.154, 0.000717062134)]
+    [InlineData(221.24, 0.13747212004)]
+    public void Double_Meter_Mile(double meter, double expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToMile();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting decimal numbers in meters to miles")]
+    [InlineData(11_423.457, 7.098204899547)]
+    [InlineData(1.154, 0.000717062134)]
+    [InlineData(221.24, 0.13747212004)]
+    public void Decimal_Meter_Mile(decimal meter, decimal expected)
+    {
+        // Arrange & Act
+        var act = meter.FromMeterToMile();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
     [Theory(DisplayName = "Converting double numbers in meters to a new unit")]
     [InlineData(2, DistanceUnit.kilometer, 0.002)]
     [InlineData(2, DistanceUnit.Meter, 2)]
--- tests/PowerUtils.Geolocation.Tests/ConversionExtensionsTests/FromMileToTests.cs
+++ tests/PowerUtils.Geolocation.Tests/ConversionExtensionsTests/FromMileToTests.cs
@@ -3,6 +3,96 @@
 [Trait("Type", "Extensions")]
 public class FromMileToTests
 {
+    [Theory(DisplayName = "Converting float numbers in miles to meters")]
+    [InlineData(11423.457, 18384226)]
+    [InlineData(1.154, 1857.17834)]
+    [InlineData(221.24, 356050.375)]
+    public void Float_Mile_Meter(float miles, float expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToMeter();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting double numbers in miles to meters")]
+    [InlineData(11423.457, 18384226.28838)]
+    [InlineData(1.154, 1857.1783599999997)]
+    [InlineData(221.24, 356050.3816)]
+    public void Double_Mile_Meter(double miles, double expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToMeter();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting decimal numbers in miles to meters")]
+    [InlineData(11423.457, 18_384_226.28838)]
+    [InlineData(1.154, 1_857.1783599999997)]
+    [InlineData(221.24, 356_050.3816)]
+    public void Decimal_Mile_Meter(decimal miles, decimal expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToMeter();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting float numbers in miles to kilometers")]
+    [InlineData(11423.457, 18384.2266)]
+    [InlineData(1.154, 1.85717833)]
+    [InlineData(221.24, 356.050385)]
+    public void Float_Mile_Kilometer(float miles, float expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting double numbers in miles to kilometers")]
+    [InlineData(11423.457, 18384.22628838)]
+    [InlineData(1.154, 1.8571783599999998)]
+    [InlineData(221.24, 356.05038160000004)]
+    public void Double_Mile_Kilometer(double miles, double expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
+    [Theory(DisplayName = "Converting decimal numbers in miles to kilometers")]
+    [InlineData(11423.457, 18384.22628838)]
+    [InlineData(1.154, 1.8571783599999998)]
+    [InlineData(221.24, 356.05038160000004)]
+    public void Decimal_Mile_Kilometer(decimal miles, decimal expected)
+    {
+        // Arrange & Act
+        var act = miles.FromMileToKilometer();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+
     [Theory(DisplayName = "Converting double numbers in miles to a new unit")]
     [InlineData(2, DistanceUnit.kilometer, 3.21_868)]
     [InlineData(2, DistanceUnit.Meter, 32_18.68)]
