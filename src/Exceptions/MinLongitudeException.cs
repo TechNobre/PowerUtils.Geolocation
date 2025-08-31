486ddc5b2fee703235f351f11e5e44a79c7b6fd3
Nelson Nobre <technobre@gmail.com>
2025-08-31 12:56:24 +0100

    refactor: remove serialization support from exceptions (Deprecated)

--- src/Exceptions/MinLongitudeException.cs
+++ src/Exceptions/MinLongitudeException.cs
@@ -1,32 +1,7 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿namespace PowerUtils.Geolocation.Exceptions;
 
-namespace PowerUtils.Geolocation.Exceptions;
-
-[Serializable]
 public class MinLongitudeException : CoordinateException
 {
     public MinLongitudeException(double coordinate)
         : base($"The minimum longitude is {Constants.MIN_LONGITUDE}. Value '{coordinate}'") { }
-
-    /// <summary>
-    /// Initializes a new instance of the exception class with serialized data.
-    /// </summary>
-    /// <param name="info">The <see cref="SerializationInfo"></see> that holds the serialized object data about the exception being thrown.</param>
-    /// <param name="context">The <see cref="StreamingContext"></see> that contains contextual information about the source or destination.</param>
-    /// <exception cref="ArgumentNullException">The <paramref name="info">info</paramref> parameter is null.</exception>
-    /// <exception cref="SerializationException">The class name is null or <see cref="P:System.Exception.HResult"></see> is zero (0).</exception>
-    protected MinLongitudeException(SerializationInfo info, StreamingContext context)
-       : base(info, context)
-    { }
-
-    public override void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-        if(info == null)
-        {
-            throw new ArgumentNullException(nameof(info));
-        }
-
-        base.GetObjectData(info, context);
-    }
 }
